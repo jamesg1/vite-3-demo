1debc417f20df84b32f361f5bc55b95821bba465
James Gosbell <3621147+jamesg1@users.noreply.github.com>
2022-10-20 15:21:20 +1000

    Added simple example test for app

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import { defineConfig } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {}
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
 })
