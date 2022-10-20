90815caead5779324e713db21b05a60836a25bd2
James Gosbell <3621147+jamesg1@users.noreply.github.com>
2022-10-20 11:03:42 +1000

    feature - Added vitest (#2)
    Added vitest with example test

---
+++ src/utils/example.test.ts
@@ -0,0 +1,17 @@
+import { assert, expect, test } from 'vitest'
+
+test('Math.sqrt()', () => {
+  expect(Math.sqrt(16)).toBe(4)
+})
+
+test('JSON', () => {
+  const input = {
+    foo: 'hi',
+    bar: 'vite',
+  }
+
+  const output = JSON.stringify(input)
+
+  expect(output).eq('{"foo":"hi","bar":"vite"}')
+  assert.deepEqual(JSON.parse(output), input, 'matches original')
+})
